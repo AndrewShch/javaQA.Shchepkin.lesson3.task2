b910f1f63bb6a21be29ba201972194eaeea2410a
Andrey Shchepkin <lovenfed@gmail.com>
2021-06-22 13:07:39 +0300

    report fixed 2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         //численные значения в копейках
-        int amount = 900_00;
+        int amount = 1200_00;
         int account = 100_00;
-        int bonus = 100;
-        int balance = amount + account;
+        int bonus;
         if (amount > 1000_00) {
-            int bonusBalance = amount / bonus;
-            int totalBalance = bonusBalance + balance;
-            System.out.println("Начислено бонусов " + bonusBalance / 100);
-            System.out.println("Баланс " + totalBalance / 100);
+            bonus =  amount / 100;
+            System.out.println("Начислено бонусов " + bonus / 100);
+
         } else {
+            bonus = 0;
             System.out.println("Бонусы не начислены");
-            System.out.println("Баланс " + balance / 100);
+
         }
+        int balance = amount + account + bonus;
+        System.out.println("Баланс " + balance / 100);
+
 
     }
 }
